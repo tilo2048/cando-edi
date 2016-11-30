34b5a5fa867cfa7f84b79a2e1088da6860583f5b
tilo2048 <tilo2048@users.noreply.github.com>
2016-11-30 22:59:58 +0100

    #1 setup Maven

---
+++ src/test/java/de/gau/cando/edi/DummyTest.java
@@ -0,0 +1,18 @@
+package de.gau.cando.edi;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DummyTest {
+
+	@Test
+	public void failure() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void success() {
+		assertTrue(true);
+	}
+}
